d32cc03b0cf300f08e34c946bcba3dee79603743
mukhammadyor17 <mukhammadyorkhon.t@evrika.com>
2025-07-15 17:22:50 +0500

    feat: add vitest and react testing library

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
@@ -6,4 +8,16 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   plugins: [tailwindcss(), react()],
   base: 'React_Tasks',
+  test: {
+    coverage: {
+      include: ['src/**/*.{js,jsx,ts,tsx}'],
+      exclude: [
+        'src/**/*.test.{js,jsx,ts,tsx}',
+        'src/**/*.spec.{js,jsx,ts,tsx}',
+        'src/index.{js,jsx,ts,tsx}',
+        'src/setupTests.{js,ts}',
+        'src/**/*.d.ts',
+      ],
+    },
+  },
 });
