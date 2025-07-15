d6ca17d12adc0cb7effd5c2cd8f39fc9275bd28a
mukhammadyor17 <mukhammadyorkhon.t@evrika.com>
2025-07-15 17:26:37 +0500

    feat: add vitest config

---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    coverage: {
+      thresholds: {
+        statements: 80,
+        branches: 50,
+        functions: 50,
+        lines: 50,
+      },
+    },
+  },
+});
