88f098753cedc2c1e20e18ea8d4ea9f455c71f2d
mukhammadyor17 <mukhammadyorkhon.t@evrika.com>
2025-08-07 21:38:04 +0500

    refactor: detail page with rtk and tests|

--- __tests__/setup.ts
+++ __tests__/setup.ts
@@ -1,18 +1,7 @@
-import { afterEach, beforeAll, afterAll } from 'vitest';
+import { afterEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/vitest';
 
-// setupTests.ts
-import { server } from '../src/queries/mocks/server';
-
-// before all tests
-beforeAll(() => server.listen());
-
-// runs a clean after each test case (e.g. clearing jsdom)
 afterEach(() => {
   cleanup();
-  server.resetHandlers();
 });
-
-// clean up after tests
-afterAll(() => server.close());
--- src/services/post_service.ts
+++ src/services/post_service.ts
@@ -1,5 +1,3 @@
-import ApiService from '../network/axios';
-
 export interface Params {
   limit: number;
   skip?: number;
@@ -9,19 +7,3 @@ export interface SearchParams {
   limit: number;
   q: string;
 }
-
-class PostService {
-  static get(params: Params) {
-    return ApiService.get('/posts', { params });
-  }
-
-  static search(params: SearchParams) {
-    return ApiService.get('/posts/search', { params });
-  }
-
-  static getById(id: string) {
-    return ApiService.get(`/posts/${id}`);
-  }
-}
-
-export default PostService;
