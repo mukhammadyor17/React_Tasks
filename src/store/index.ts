5ec4fd1db4df819730e42e26de60394caec6fcaa
mukhammadyor17 <mukhammadyorkhon.t@evrika.com>
2025-08-07 18:13:50 +0500

    feat: create rtk api for fetching data

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { postApi } from './query/post_api';
 import favoritesReducer from './features/favorites/favorites_slice';
 
 export const store = configureStore({
   reducer: {
     favorites: favoritesReducer,
+    [postApi.reducerPath]: postApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(postApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
