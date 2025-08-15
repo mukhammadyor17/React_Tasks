24d046124c32c95956e920ca15b5071897562b23
mukhammadyor17 <mukhammadyorkhon.t@evrika.com>
2025-08-15 23:15:55 +0500

    change: next conf

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
   // output: 'export', // Outputs a Single-Page Application (SPA).
-  distDir: './dist', // Changes the build output directory to `./dist/`.
+  // distDir: './dist', // Changes the build output directory to `./dist/`.
   basePath: process.env.NEXT_PUBLIC_BASE_PATH, // Sets the base path to `/some-base-path`.
 };
 
