50a6fcca688d00babf37627592e5ba175bd50315
mukhammadyor17 <mukhammadyorkhon.t@evrika.com>
2025-08-30 13:13:08 +0500

    feat: add profiler

--- react-performance/src/App.tsx
+++ react-performance/src/App.tsx
@@ -1,20 +1,28 @@
-import { Suspense } from 'react';
+import { Suspense, Profiler } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Loader from './components/Loader';
 import CountryTable from './components/CountryTable';
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <Suspense fallback={<Loader />}>
-      <div className="w-full h-full p-10">
-        <div className="max-w-5xl mx-auto bg-gray-100 p-5 rounded-md">
-          <CountryTable />
+const App = () => {
+  const onRender = () => {
+    console.log('CountryTable rendered');
+  };
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Suspense fallback={<Loader />}>
+        <div className="w-full h-full p-10">
+          <div className="max-w-5xl mx-auto bg-gray-100 p-5 rounded-md">
+            <Profiler id="CountryTable" onRender={onRender}>
+              <CountryTable />
+            </Profiler>
+          </div>
         </div>
-      </div>
-    </Suspense>
-  </QueryClientProvider>
-);
+      </Suspense>
+    </QueryClientProvider>
+  );
+};
 
 export default App;
